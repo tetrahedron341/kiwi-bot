22a934e9907514a6388ca5271512929eb911d003
Ben <bengdahl341@gmail.com>
2018-08-24 16:17:24 -0400

    First version

---
+++ config.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+
+fs.readFile("./config.json", "utf8", (err, data) => {
+    if (err!=null) {
+        throw err;
+    }
+    module.exports = JSON.parse(data);
+});
---
+++ index.js
@@ -0,0 +1,33 @@
+const discord = require("discord.js");
+const client = new discord.Client();
+const delay = require("delay");
+
+const config = require("./config.js");
+
+async function waitForKiwiHours(){ // jshint ignore: line
+    var current = new Date(Date.now());
+    //current.setDate(current.getDate()-4);
+    var kiwihours = new Date(current.getFullYear(),
+        current.getMonth(),
+        current.getDate() + 1,
+        1,
+        40);
+    if (kiwihours.getDay() == 2) {
+        kiwihours.setDate(kiwihours.getDate() + 1);
+    }
+    console.log("It is currently " + current.toUTCString());
+    console.log("Waiting until " + kiwihours.toUTCString());
+    console.log("Waiting for " + (kiwihours.getTime() - current.getTime()) + " ms");
+    await delay(5000); // jshint ignore: line
+    return 0;
+}
+
+client.on('ready', () => {
+    console.log("discord.js client ready.");
+    waitForKiwiHours().then(()=>{
+
+    }).catch(()=>{
+        
+    });
+    console.log("Kiwi-bot is ready.");
+});
