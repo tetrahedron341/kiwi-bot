59f7aa3c1692ce3325df357cb03fa3a65c4f6254
Ben <bengdahl341@gmail.com>
2018-08-25 18:19:07 -0400

    Add events & commands manager

--- index.js
+++ index.js
@@ -1,17 +1,58 @@
+const fs = require("fs");
 const discord = require("discord.js");
 const client = new discord.Client();
 const delay = require("delay");
+const Enmap = require("enmap");
 
-const config = require("./config");
-const kiwiHours = require("./kiwiHours");
+client.loadedModules = new Set();
 
-client.config = config;
+client.initialize = function() {
+    const config = require("./config");
+    client.loadedModules.add("./config");
+    const kiwiHours = require("./kiwiHours");
+    client.loadedModules.add("./kiwiHours");
+    
+    client.config = config;
+    kiwiHours(client);
+
+    fs.readdir("./events/", (err, files) => {
+        if (err) return console.error(err);
+        files.forEach((file)=>{
+            const event = require(`./events/${file}`);
+            client.loadedModules.push(`./events/${file}`); 
+            let eventName = file.split(".")[0];
+            client.on(eventName, event.bind(null, client));
+        });
+    });
+
+    client.commands = new Enmap();
+
+    fs.readdir("./commands/", (err, files) => {
+        if (err) return console.error(err);
+        files.forEach((file) =>{
+            if (!file.endsWith(".js")) return;
+            let mod = require(`./commands/${file}`);
+            client.loadedModules.push(`./commands/${file}`);
+            let commandName = file.split('.')[0];
+            console.log(`Loading command: ${commandName}`);
+            client.commands.set(commandName, mod);
+        });
+    });
+};
+
+client.reload = function() {
+    client.loadedModules.forEach(mod => {
+        delete require.cache[require.resolve("mod")];
+    });
+    
+    delete client.commands;
+    
+    client.initialize();
+};
 
 client.on('ready', () => {
     console.log("discord.js client ready.");
     console.log(client.guilds.keyArray());
-    kiwiHours(client);
-    console.log("Kiwi-bot is ready.");
 });
 
 client.login(config.key);
