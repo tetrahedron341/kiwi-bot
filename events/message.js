241fb5c603e1d15f39c8f5e65c6bb729d1dd72df
Ben <bengdahl341@gmail.com>
2018-08-25 19:00:04 -0400

    Add command handler

--- events/message.js
+++ events/message.js
@@ -1,3 +1,16 @@
 module.exports = (client, message) => {
-    console.log(message.content);
+    console.log(`[(${message.guild.name},${message.channel.name}), ${message.author.username}]: ${message.content}`);
+    
+    if (message.author.bot) return;
+    let prefix = client.config.getServerSettings().prefix;
+    if (message.content.startsWith(prefix)){
+        let args = message.content.substring(prefix.length).split(" ");
+        let command = args.shift().toLowerCase();
+        
+        let cmd = client.commands.get(command);
+
+        if (!cmd) return;
+
+        cmd.run(client, message, args);
+    }
 };
