cd6f2004d300a79c14e8caa0a8867a8b78c422d8
Ben <bengdahl341@gmail.com>
2018-08-25 19:08:40 -0400

    ping pong

--- events/message.js
+++ events/message.js
@@ -2,10 +2,14 @@ module.exports = (client, message) => {
     console.log(`[(${message.guild.name},${message.channel.name}), ${message.author.username}]: ${message.content}`);
     
     if (message.author.bot) return;
-    let prefix = client.config.getServerSettings().prefix;
-    if (message.content.startsWith(prefix)){
+    let prefix = client.config.getServerSettings(message.guild.id).prefix;
+    console.log(client.config.getServerSettings(message.guild.id));
+    
+    if (message.content.startsWith(prefix)){        
         let args = message.content.substring(prefix.length).split(" ");
         let command = args.shift().toLowerCase();
+
+        console.log(`Command ${command} run with args ${args}`);
         
         let cmd = client.commands.get(command);
 
